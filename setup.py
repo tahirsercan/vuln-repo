c57e9d18ab384a85297fbc037e638b7a658cb9fd
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-06 20:29:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,3 +4,11 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup()
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
